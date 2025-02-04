eb7f2a9929231ad3bda30bc34c4e042adb0cea1c
Dasha <daryafralova@gmail.com>
2025-02-04 22:08:46 +0300

    04.02.2025

---
+++ SQL/016_test.sql
@@ -0,0 +1,10 @@
+---For this challenge you need to create a simple SUM statement that will sum all the ages.
+
+---people table schema
+---id
+---name
+---age
+
+
+select sum(age) as age_sum
+from people
---
+++ SQL/018_test.sql
@@ -0,0 +1,11 @@
+---Given the following table 'decimals':
+
+---** decimals table schema **
+
+---id
+---number1
+---number2
+---Return a table with two columns (number1, number2), the value in number1 should be rounded down and the value in number2 should be rounded up.
+
+select floor(number1) as number1, ceil(number2) as number2
+from decimals
